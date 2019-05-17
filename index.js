708d7a186c80ffd830debacb190441673cec483e
Carlos Diego Quirino Lima <diegoquirino@gmail.com>
2019-05-16 23:50:37 -0300

    Implementado o salvamento do arquivo com a substituição das variáveis

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs')
 const { check, validationResult } = require('express-validator/check');
 
 const app = express();
@@ -34,6 +35,8 @@ app.post("/download", [
     check('baseline','A Baseline deve ser número inteiro maior ou igual a (1) um!').isInt({min:1})
 ], (req, res) => {
     baseplan = req.body;
+    baseplan.filename = baseplan.sistema.replace(/[^A-Z0-9]+/ig, "_") + "-v"+ baseplan.versao + "_b" + baseplan.baseline + ".txt"
+
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         req.body.page = 'pages/home.ejs';
@@ -43,6 +46,19 @@ app.post("/download", [
         req.body.page = 'pages/download.ejs';
         req.body.errors = false;
         req.body.baseplan = baseplan;
+        readFile("./public/organizartestes.txt")
+            .then(data => {
+                data = data.toString().replace(/{sistema}/g, baseplan.sistema);
+                data = data.toString().replace(/{versao}/g, baseplan.versao);
+                data = data.toString().replace(/{baseline}/g, baseplan.baseline);
+                writeFile("./public/files/" + baseplan.filename, data);
+            })
+            .then(() => console.log("Arquivo "+ baseplan.filename + " criado com sucesso!"))
+            .catch(err => {
+                req.body.page = 'pages/home.ejs';
+                req.body.errors = err;
+                req.body.baseplan = baseplan;
+            })
     }
     res.render("layout", req.body);
 })
@@ -50,3 +66,23 @@ app.post("/download", [
 ////////
 app.listen(PORT, HOST);
 console.log("Organiza started at http://localhost:"+PORT);
+
+//////// Functions ////////
+const readFile = path => new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+        if(err) {
+            reject(err);
+        } else {
+            resolve(data);
+        }
+    })
+});
+const writeFile = (path, data) => new Promise((resolve, reject) => {
+    fs.writeFile(path, data, (err) => {
+        if(err) {
+            reject(err);
+        } else {
+            resolve();
+        }
+    })
+});
